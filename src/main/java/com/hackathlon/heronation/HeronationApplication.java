5ac7399a6c9cc7646d0984bcba2152e0cdcbef01
Jose <jmepicapp@gmail.com>
2020-04-25 17:54:57 +0200

    first commit

---
+++ src/main/java/com/hackathlon/heronation/HeronationApplication.java
@@ -0,0 +1,13 @@
+package com.hackathlon.heronation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeronationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeronationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hackathlon/heronation/HeronationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackathlon.heronation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeronationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
