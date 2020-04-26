9938aec153cbf5312a4d8df873b899aaa2f94fa7
Jose <jmepicapp@gmail.com>
2020-04-26 10:28:17 +0200

    ComunidadAutonoma as entity, poblacion model issues

--- src/main/java/com/hackathlon/heronation/model/enumeration/ComunidadAutonoma.java
+++
@@ -1,8 +0,0 @@
-package com.hackathlon.heronation.model.enumeration;
-
-/**
- * The ComunidadAutonoma enumeration.
- */
-public enum ComunidadAutonoma {
-    ANDALUCIA, ARAGON, BALEARES, CATALUNA, CANARIAS, CANTABRIA, CASTILLALAMANCHA, CASTILLALEON, MADRID, NAVARRA, COMUNIDADVALENCIANA, EXTREMADURA, GALICIA, PAISVASCO, ASTURIAS, MURCIA, LARIOJA
-}
