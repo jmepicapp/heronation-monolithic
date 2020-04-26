0bd711f1c69c5d9463f983ebc364a83404d8a818
ferohe27 <lromeroherazo@gmail.com>
2020-04-26 21:01:17 +0200

    Se quita propiedad categoria empresa

--- src/main/java/com/hackathlon/heronation/model/UsuarioEmpresa.java
+++ src/main/java/com/hackathlon/heronation/model/UsuarioEmpresa.java
@@ -52,10 +52,6 @@ public class UsuarioEmpresa implements Serializable {
     @JoinColumn(name = "usuario_id", referencedColumnName = "id")
     private Usuario usuarioEmpresas;
 
-    @ManyToMany(mappedBy = "usuarioEmpresas")
-    @JsonIgnore
-    private Set<CategoriaEmpresa> categoriaEmpresas = new HashSet<>();
-
     // jhipster-needle-entity-add-field - JHipster will add fields here, do not remove
     public Long getId() {
         return id;
@@ -192,32 +188,6 @@ public class UsuarioEmpresa implements Serializable {
     public void setPeticionUsuarioEmpresas(Set<Peticion> peticions) {
         this.peticionUsuarioEmpresas = peticions;
     }
-
-    public Set<CategoriaEmpresa> getCategoriaEmpresas() {
-        return categoriaEmpresas;
-    }
-
-    public UsuarioEmpresa categoriaEmpresas(Set<CategoriaEmpresa> categoriaEmpresas) {
-        this.categoriaEmpresas = categoriaEmpresas;
-        return this;
-    }
-
-    public UsuarioEmpresa addCategoriaEmpresa(CategoriaEmpresa categoriaEmpresa) {
-        this.categoriaEmpresas.add(categoriaEmpresa);
-        categoriaEmpresa.getUsuarioEmpresas().add(this);
-        return this;
-    }
-
-    public UsuarioEmpresa removeCategoriaEmpresa(CategoriaEmpresa categoriaEmpresa) {
-        this.categoriaEmpresas.remove(categoriaEmpresa);
-        categoriaEmpresa.getUsuarioEmpresas().remove(this);
-        return this;
-    }
-
-    public void setCategoriaEmpresas(Set<CategoriaEmpresa> categoriaEmpresas) {
-        this.categoriaEmpresas = categoriaEmpresas;
-    }
-    // jhipster-needle-entity-add-getters-setters - JHipster will add getters and setters here, do not remove
 
     @Override
     public boolean equals(Object o) {
