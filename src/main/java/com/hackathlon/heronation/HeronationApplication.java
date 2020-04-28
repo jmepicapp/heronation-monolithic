8293728bf4a88c95d467e4ebb05814ec6c303e78
Jose <jmepicapp@gmail.com>
2020-04-28 02:44:11 +0200

    Spring security configuration (login)

--- src/main/java/com/hackathlon/heronation/HeronationApplication.java
+++ src/main/java/com/hackathlon/heronation/HeronationApplication.java
@@ -1,13 +1,28 @@
 package com.hackathlon.heronation;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
-public class HeronationApplication {
+public class HeronationApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HeronationApplication.class, args);
 	}
+//
+//	@Autowired
+//	private BCryptPasswordEncoder passwordEncoder;
 
+	@Override
+	public void run(String... args) throws Exception {
+//		String password = "12345";
+//
+//		for (int i = 0; i < 5; i++){
+//			String passwordBcrypt = passwordEncoder.encode(password);
+//			System.out.println(passwordBcrypt);
+//		}
+	}
 }
