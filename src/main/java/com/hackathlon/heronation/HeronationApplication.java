21ab0a83a158a3c6308760c190d2c74bf89d7d05
ferohe27 <lromeroherazo@gmail.com>
2020-04-28 09:06:17 +0200

    avances trigger

--- src/main/java/com/hackathlon/heronation/HeronationApplication.java
+++ src/main/java/com/hackathlon/heronation/HeronationApplication.java
@@ -4,7 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HeronationApplication {
+@EnableScheduling
+@EnableSchedulerLock(defaultLockAtMostFor = "30s")
+public class HeronationApplication{
 
 	public static void main(String[] args) {
 		SpringApplication.run(HeronationApplication.class, args);
