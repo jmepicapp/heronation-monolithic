9a9167476988318b1f5165a80fdff5ab62d637e7
Jose <jmepicapp@gmail.com>
2020-04-29 02:28:32 +0200

    permisos preferencias

--- src/main/java/com/hackathlon/heronation/auth/ResourceServerConfig.java
+++ src/main/java/com/hackathlon/heronation/auth/ResourceServerConfig.java
@@ -21,8 +21,8 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
     @Override
     public void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().antMatchers(HttpMethod.GET, "/api/usuario-empresas", "/api/usuario-empresas/{id}", "/api/categoria-productos", "/api/preferencias/categoria-producto").permitAll()
-                .antMatchers(HttpMethod.POST, "/api/usuario-empresas", "/api/usuario-donantes").permitAll()
+        http.authorizeRequests().antMatchers(HttpMethod.GET, "/api/usuario-empresas", "/api/usuario-empresas/{id}", "/api/categoria-productos").permitAll()
+                .antMatchers(HttpMethod.POST, "/api/usuario-empresas", "/api/usuario-donantes", "/api/preferencias/categoria-producto").permitAll()
                 .antMatchers(HttpMethod.GET, "/api/donacion").hasAnyRole("DONANTE", "EMPRESA", "ADMIN")
                 .anyRequest().authenticated()
                 .and().cors().configurationSource(corsConfigurationSource());
